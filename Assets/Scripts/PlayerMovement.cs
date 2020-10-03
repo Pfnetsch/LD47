de4368786b8352f35326c556d4270c578b46d9e3
Patrick Seebacher <patrick.seebacher@gmail.com>
2020-10-03 21:27:17 +0200

    Player Movement fixed - Solar Planet Movement set to 0

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,9 @@ public class PlayerMovement : MonoBehaviour
     public Transform leftBorder;
     public Transform rightBorder;
 
-    public float movementSpeed = 0.5F;
+    public float initialMovementSpeed = 0.2F;
+    private float _movementSpeed;
+
     public bool isJumpingAllowed = false;
 
 
@@ -23,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveIncrDelayInSec = 2.0F;
     private int _movementDirection = 0; // 0 standing still, 1 is left, 2 is right
     private float _movementTimer = 0.0F;
-    private int _incCounter = 1;
+    private int _movementIncrementCounter = 1;
 
     // Direction to the next Planet
 
@@ -34,60 +36,57 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _movementSpeed = initialMovementSpeed;
         _playerBody = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && player.GetComponent<Player>().isGrounded)
         {
             if (_movementDirection != 1)
             {
-                _movementTimer = 0.0f; // Timer Reset
-                _movementDirection = 1;
+                ResetMovementSpeed(1);
             }
 
-            if (player.transform.position.x > leftBorder.position.x && (player.transform.position.y > leftBorder.position.y || player.transform.position.x > 0F))
+            if (player.transform.position.x > leftBorder.position.x && (player.transform.position.y > leftBorder.position.y || player.transform.localPosition.x > 0F))
             {
                 // rotate player
-                player.transform.RotateAround(currentPlanet.position, Vector3.forward, movementSpeed);
+                player.transform.RotateAround(currentPlanet.position, Vector3.forward, _movementSpeed);
             }
             else
             {
                 // rotate planet
-                allPlanets.RotateAround(currentPlanet.position, Vector3.forward, movementSpeed * -1F);
+                allPlanets.RotateAround(currentPlanet.position, Vector3.forward, _movementSpeed * -1F);
             }
         }
         else if (_movementDirection == 1)   // Left Key Up
         {
-            _movementDirection = 0;
-            _movementTimer = 0.0f; // Timer Reset
+            ResetMovementSpeed(0);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && player.GetComponent<Player>().isGrounded)
         {
             if (_movementDirection != 2)
             {
-                _movementTimer = 0.0f; // Timer Reset
-                _movementDirection = 2;
+                ResetMovementSpeed(2);
             }
 
-            if (player.transform.position.x < rightBorder.position.x && (player.transform.position.y > rightBorder.position.y || player.transform.position.x < 0F))
+            if (player.transform.position.x < rightBorder.position.x && (player.transform.position.y > rightBorder.position.y || player.transform.localPosition.x < 0F))
             {
                 // rotate player
-                player.transform.RotateAround(currentPlanet.position, Vector3.forward, movementSpeed * -1F);
+                player.transform.RotateAround(currentPlanet.position, Vector3.forward, _movementSpeed * -1F);
             }
             else
             {
                 // rotate planet
-                allPlanets.RotateAround(currentPlanet.position, Vector3.forward, movementSpeed);
+                allPlanets.RotateAround(currentPlanet.position, Vector3.forward, _movementSpeed);
             }
         }
         else if (_movementDirection == 2)
         {
-            _movementDirection = 0;
-            _movementTimer = 0.0f; // Timer Reset
+            ResetMovementSpeed(0);
         }
 
         if (isJumpingAllowed && Input.GetKeyDown(KeyCode.Space) && player.GetComponent<Player>().isGrounded)
@@ -99,13 +98,20 @@ public class PlayerMovement : MonoBehaviour
 
         if (_movementDirection != 0 && moveIncrDelayInSec != 0 && _movementTimer > moveIncrDelayInSec)
         {
-            movementSpeed += movementSpeed * _incCounter;
+            _movementSpeed += initialMovementSpeed * _movementIncrementCounter * _movementIncrementCounter;
+            _playerBody.gravityScale -= 0.01F * _movementIncrementCounter * _movementIncrementCounter;
             _movementTimer = 0.0F;
-        }
+            _movementIncrementCounter++;
 
-        if (movementSpeed > 100)
-        {
-
+            UnityEngine.Debug.Log("GravityScale: " + _playerBody.gravityScale);
         }
+    }
+
+    private void ResetMovementSpeed(int movementDirection)
+    {
+        _movementDirection = movementDirection;
+        _movementSpeed = initialMovementSpeed;
+        _movementTimer = 0.0f; // Timer Reset
+        _movementIncrementCounter = 1;
     }
 }
