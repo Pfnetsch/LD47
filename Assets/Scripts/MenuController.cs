ce8001a72356169f5b6a698231cee3c9a5524d4f
Grundplatte <grundplattexox@gmail.com>
2020-10-03 13:59:00 +0200

    Added audio mixer and main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("Game").buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsController : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMainVolume(float value)
+    {
+        mainMixer.SetFloat("MainVolume", value);
+    }
+    
+    public void SetMusicVolume(float value)
+    {
+        mainMixer.SetFloat("MusicVolume", value);
+    }
+}
