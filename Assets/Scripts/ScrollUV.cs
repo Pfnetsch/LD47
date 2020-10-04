43e4a4726864b29e37e8f6af8bf63bf995efd5f9
Grundplatte <grundplattexox@gmail.com>
2020-10-04 13:15:02 +0200

    Added Transition Scene

---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Material mat = GetComponent<MeshRenderer>().material;
+        
+        mat.mainTextureOffset += new Vector2(Time.deltaTime / 10F, Time.deltaTime / 10F);
+    }
+}
