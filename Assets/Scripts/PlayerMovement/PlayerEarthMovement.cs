a88f6fa995725f1c8edb9284dd566e7ec82b4750
Patrick Seebacher <patrick.seebacher@gmail.com>
2020-10-04 21:57:41 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Pfnetsch/LD47 into main""
    This reverts commit 72462bb4061693551947c797d4090c5e48ede9d7.

--- Assets/Scripts/PlayerMovement/PlayerEarthMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerEarthMovement.cs
@@ -24,6 +24,11 @@ public class PlayerEarthMovement : MonoBehaviour, IPlayerPlanetMovement
         _teleportTransform.GetComponent<Animator>().SetTrigger("startTP");
     }
 
+    public void PlayerSetup(GameObject rootGameObject)
+    {
+        rootGameObject.GetComponentInChildren<Camera>().orthographicSize = 5;
+    }
+
     public void PlayerUpdate(Rigidbody2D playerBody)
     {
         //Debug.Log("Earth Movement");
